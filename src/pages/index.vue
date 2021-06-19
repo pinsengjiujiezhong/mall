33f56d6fbd8159a8d2475b748c4e85ae2c5a853a
unknown <liguang.yang@tuya.com>
2021-06-19 11:10:25 +0800

    'feat:项目目录结构提交'

---
+++ src/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <nav-header></nav-header>
+    <router-view></router-view>
+    <nav-fooder></nav-fooder>
+  </div>
+</template>
+
+<script>
+export default {
+name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>
